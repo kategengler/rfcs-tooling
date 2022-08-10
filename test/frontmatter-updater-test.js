3497de92fb439fa18a93a180c32c1ef5690820ba
Katie Gengler <katie@kmg.io>
2022-08-09 23:10:32 -0400

    Add script to update stage of RFCs
    - Add class to generally update frontmatter

---
+++ test/frontmatter-updater-test.js
@@ -0,0 +1,86 @@
+const expect = require('chai').expect;
+const FrontmatterUpdater = require('../lib/frontmatter-updater');
+
+const baseMarkdown = `---
+stage: accepted
+start-date:
+release-date: Unreleased
+release-versions:
+  ember-source: vX.Y.Z
+  ember-data: vX.Y.Z
+teams:
+  - framework
+prs:
+  accepted:
+---
+# My RFC
+
+## My heading
+
+Foo bar baz
+`;
+
+const readyForReleaseMarkdown = `---
+stage: ready-for-release
+start-date:
+release-date: Unreleased
+release-versions:
+  ember-source: vX.Y.Z
+  ember-data: vX.Y.Z
+teams:
+  - framework
+prs:
+  accepted:
+---
+# My RFC
+
+## My heading
+
+Foo bar baz
+`;
+
+const updatedDeepKeysMarkdown = `---
+stage: ready-for-release
+start-date:
+release-date: Unreleased
+release-versions:
+  ember-source: v1.2.3
+  ember-data: v4.5.6
+  ember-cli: v7.8.9
+teams:
+  - framework
+  - ember-data
+prs:
+  accepted:
+  ready-for-release: 'https:://github.com/emberjs/rfcs/pull/001'
+---
+# My RFC
+
+## My heading
+
+Foo bar baz
+`;
+
+describe('FrontmatterUpdater', function () {
+  it('updates the frontmatter based on metadata passed', function () {
+    let updater = new FrontmatterUpdater(baseMarkdown);
+    let output = updater.updateMetadata({ stage: 'ready-for-release' });
+    expect(output).to.eql(readyForReleaseMarkdown);
+  });
+
+  it('updates the frontmatter when deep values are included', function () {
+    let updater = new FrontmatterUpdater(readyForReleaseMarkdown);
+    let output = updater.updateMetadata({
+      'release-versions': {
+        'ember-source': 'v1.2.3',
+        'ember-data': 'v4.5.6',
+        'ember-cli': 'v7.8.9',
+      },
+      prs: {
+        'ready-for-release': 'https:://github.com/emberjs/rfcs/pull/001',
+      },
+      teams: ['framework', 'ember-data'],
+    });
+    expect(output).to.eql(updatedDeepKeysMarkdown);
+  });
+});
