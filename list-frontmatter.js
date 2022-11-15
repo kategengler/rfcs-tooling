4dd16f396c6b44d54face3114062f78143258b54
Katie Gengler <katie@kmg.io>
2022-11-15 14:36:28 -0500

    Add util to list frontmatter from all

---
+++ list-frontmatter.js
@@ -0,0 +1,25 @@
+const argv = require('yargs').command('* [paths..]', 'list frontmatter from files', (yargs) => {
+  return yargs
+    .positional('paths', {
+      describe: 'file paths to list frontmatter from',
+      type: 'array',
+    })
+    .demandOption('paths');
+}).argv;
+
+const frontmatter = require('@github-docs/frontmatter');
+const { readFileSync } = require('fs');
+
+let results = [];
+for (let path of argv.paths) {
+  let markdown = readFileSync(path, 'utf8');
+  const { data, errors } = frontmatter(markdown);
+  if (errors.length) {
+    console.error(JSON.stringify(errors));
+    process.exitCode = 1;
+    return;
+  }
+  results.push({ name: path, ...data });
+}
+
+console.log(JSON.stringify(results, null, 2));
